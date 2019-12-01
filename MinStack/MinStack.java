8e131189ff5885dca340aba99bbbcc8c7c96ed86
OEM Configuration (temporary user) <bamichae@buffalo.edu>
2019-12-01 11:09:52 -0500

    adding MinStack completed files

--- MinStack/MinStack.java
+++ MinStack/MinStack.java
@@ -1,4 +1,51 @@
-package PACKAGE_NAME;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
 
-public class MinStack {
+class MinStack {
+
+    private PriorityQueue<Integer> minQueue = new PriorityQueue<>();
+    private List<Integer> stack = new LinkedList<>();
+
+    public MinStack() {
+
+    }
+
+    public void push(int x) {
+        stack.add(x);
+        minQueue.add(x);
+    }
+
+    public void pop() {
+        Integer element = null;
+        if (!stack.isEmpty()) {
+           element = stack.remove(stack.size() - 1);
+        }
+        if (!minQueue.isEmpty()) {
+            minQueue.remove(element);
+        }
+    }
+
+    public Integer top() {
+        if (!stack.isEmpty()) {
+            return stack.get(stack.size() - 1);
+        }
+        return null;
+    }
+
+    public Integer getMin() {
+        if (!minQueue.isEmpty()) {
+            return minQueue.peek();
+        }
+        return null;
+    }
 }
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
--- Test/TestMinStack.java
+++ Test/TestMinStack.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import org.junit.Test;
 
 public class TestMinStack {
+    @Test
+    public void testPush()
+    {
+        MinStack minStack = new MinStack();
+        minStack.push(2);
+        minStack.push(-3);
+        minStack.push(0);
+        minStack.push(40);
+        System.out.println(minStack.top());
+        System.out.println(minStack.getMin());
+        minStack.pop();
+        System.out.println(minStack.top());
+        System.out.println(minStack.getMin());
+    }
 }
