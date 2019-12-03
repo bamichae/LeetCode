1f37c9708e373504dca070d079ef553f5b5bd3f9
OEM Configuration (temporary user) <bamichae@buffalo.edu>
2019-12-02 21:42:52 -0500

    wrong answer because misleading question. Skipping question

---
+++ CountAndSay/CountAndSay.java
@@ -0,0 +1,65 @@
+public class CountAndSay
+{
+
+
+    public String countAndSay(int n)
+    {
+        if (n == 1)
+        {
+            return "1";
+        }
+        String finalString = "1";
+        for (int i = 2; i <= n; i++)
+        {
+            finalString = calculate(finalString);
+        }
+        return finalString;
+    }
+
+    public String calculate(String inString)
+    {
+        String nthNum = "";
+        for (int i = 0; i < inString.length(); i++)
+        {
+            int peekNum = i + 1;
+            if (peekNum >= inString.length())
+            {
+                nthNum += decodeNum(inString.substring(i, i + 1));
+            } else
+            {
+                if (!"invalid".equals(decodeNums(inString.substring(i, i + 1), inString.substring(peekNum, peekNum + 1))))
+                {
+                    nthNum += decodeNums(inString.substring(i, i + 1), inString.substring(peekNum, peekNum + 1));
+                    i++;
+                } else
+                {
+                    nthNum += decodeNum(inString.substring(i, i + 1));
+                }
+            }
+        }
+        return nthNum;
+    }
+
+    private String decodeNums(String first, String second)
+    {
+        return decodeNum(first + second);
+    }
+
+    private String decodeNum(String n)
+    {
+        if ("1".equals(n))
+        {
+            return "11";
+        } else if ("11".equals(n))
+        {
+            return "21";
+        } else if ("2".equals(n))
+        {
+            return "12";
+        } else
+        {
+            return "invalid";
+        }
+    }
+
+}
---
+++ Test/TestCountAndSay.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+public class TestCountAndSay
+{
+    @Test
+    public void testCountAndSay()
+    {
+        CountAndSay countAndSay = new CountAndSay();
+        System.out.println("result: " + countAndSay.calculate("1211"));
+    }
+}
