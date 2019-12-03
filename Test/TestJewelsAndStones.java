59ccf81f888a5a0bc123a0a557a19cd2833ed7b2
OEM Configuration (temporary user) <bamichae@buffalo.edu>
2019-12-03 01:13:19 -0500

    working solution for 771 Jewels and Stones

---
+++ Test/TestJewelsAndStones.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+public class TestJewelsAndStones
+{
+    @Test
+    public void testJewelsAndStones()
+    {
+        JewelsAndStones jewelsAndStones = new JewelsAndStones();
+        System.out.println(jewelsAndStones.numJewelsInStones("dZz", "dDzZz"));
+    }
+}
