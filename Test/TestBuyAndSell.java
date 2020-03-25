503c855c470de376b5e785cf6c0dc1fdc9b1e332
OEM Configuration (temporary user) <bamichae@buffalo.edu>
2020-03-25 02:10:57 -0400

    number add

--- Test/TestBuyAndSell.java
+++ Test/TestBuyAndSell.java
@@ -1,5 +1,14 @@
-package PACKAGE_NAME;
+import BuyAndSell.BuyAndSell;
+import org.junit.Test;
 
 public class TestBuyAndSell
 {
+    @Test
+    public void testBuyAndSell()
+    {
+        int[] maxProfit = {7, 1, 5, 3, 6, 4};
+        BuyAndSell buyAndSell = new BuyAndSell();
+        int max = buyAndSell.maxProfit(maxProfit);
+        System.out.println(max);
+    }
 }
--- Test/TestCountAndSay.java
+++ Test/TestCountAndSay.java
@@ -1,5 +1,5 @@
 import org.junit.Test;
-
+import CountAndSay.CountAndSay;
 public class TestCountAndSay
 {
     @Test
